30e87fe1e077320a6cd96b656d6f8ca80f3954c0
Grecia Corona <grecia.corona@gmail.com>
2020-10-16 14:23:17 -0500

    Conversor de monedas de tres divisas usando condicionales

--- conversor_de_monedas.py
+++ conversor_de_monedas.py
@@ -1,7 +1,37 @@
-pesos = input("¿Cuántos pesos mexicanos tienes? ")
-pesos = float(pesos)
-valor_dolar = 21.18
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dólares")
+menu = """
+Bienvenido al conversor de monedas 💱
+
+1 - Pesos Colombianos
+2 - Pesos Argentinos
+3 - Pesos Mexicanos
+
+Elige una opción: """
+
+opcion = int(input(menu))
+if opcion == 1:
+    pesos = input("¿Cuántos pesos colombianos tienes? ")
+    pesos = float(pesos)
+    valor_dolar = 3845
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 2:
+    pesos = input("¿Cuántos pesos argentinos tienes? ")
+    pesos = float(pesos)
+    valor_dolar = 77.49
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 3:
+    pesos = input("¿Cuántos pesos mexicanos tienes? ")
+    pesos = float(pesos)
+    valor_dolar = 21.13
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+else:
+    print("Ingresa una opción correcta")
+
