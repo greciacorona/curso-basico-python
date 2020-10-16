a0c915887b48d14b6670aa657714cec8d822edde
Grecia Corona <grecia.corona@gmail.com>
2020-10-15 19:16:17 -0500

    Mi primer programa en Python :D

---
+++ conversor_de_monedas.py
@@ -0,0 +1,7 @@
+pesos = input("¿Cuántos pesos mexicanos tienes? ")
+pesos = float(pesos)
+valor_dolar = 21.29
+dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
+dolares = str(dolares)
+print("Tienes $" + dolares + " dólares")
