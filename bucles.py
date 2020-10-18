862a74862b06d18995dc1a7f804794665f74c7fa
Grecia Corona <grecia.corona@gmail.com>
2020-10-17 22:58:53 -0500

    Potencia de 2 cuyo resultado de menos de 1000

--- bucles.py
+++ bucles.py
@@ -1,9 +1,13 @@
 def run():
-    i = 2
-    while i <= 1000:
-        print(2**i)
-        i += 1
-        
+    LIMITE = 1000
 
-if __name__ == "__main__":
+    contador = 0
+    potencia_2 = 2 ** contador
+    while potencia_2 < LIMITE:
+        print("2 elevado a " + str(contador) + "es igual a " + str(potencia_2))
+        contador += 1
+        potencia_2 = 2**contador
+
+
+if  __name__ == "__main__":
     run()
