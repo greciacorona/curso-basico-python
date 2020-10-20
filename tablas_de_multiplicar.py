27039877bcfadfe5cdc83c83274e0541c98b1cb0
Grecia Corona <grecia.corona@gmail.com>
2020-10-20 12:08:53 -0500

    Correción del código con el uso de cadenas f

--- tablas_de_multiplicar.py
+++ tablas_de_multiplicar.py
@@ -1,7 +1,7 @@
 def run():
     tabla = input("¿Qué tabla de multiplicar quieres ver?")
     for i in range(11):
-        print("2 X " + str(i) + " = " + str(int(tabla) * i))
+        print(f"2 X {i} = {int(tabla) * i}")
 
 
 if __name__ == '__main__':
